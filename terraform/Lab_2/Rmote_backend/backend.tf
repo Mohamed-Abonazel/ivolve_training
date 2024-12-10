3afdf2f87175dffbf948ebd734696706f6d4af5f
Mohamed K. Abonazel <mohamedabonazel4@gmail.com>
2024-12-11 00:46:51 +0200

    Update backend.tf

--- terraform/Lab_2/Rmote_backend/backend.tf
+++ terraform/Lab_2/Rmote_backend/backend.tf
@@ -1,1 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "ivolve-s3"   # Replace with your bucket name
+    key    = "terraform/state.tfstate"
+    region = "us-east-1"                     # Replace with your preferred AWS region
+  }
+}
+
 
