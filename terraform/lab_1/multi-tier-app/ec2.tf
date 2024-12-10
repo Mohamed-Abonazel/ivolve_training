6560446949d75084e0653daefdea485569077829
Mohamed K. Abonazel <mohamedabonazel4@gmail.com>
2024-12-11 00:10:51 +0200

    Update ec2.tf

--- terraform/lab_1/multi-tier-app/ec2.tf
+++ terraform/lab_1/multi-tier-app/ec2.tf
@@ -1,1 +1,15 @@
+resource "aws_instance" "app_server" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.app_subnet.id
+  associate_public_ip_address = true  # This ensures the EC2 instance gets a public IP
+  tags = {
+    Name = "App-Server"
+  }
+
+  # Local provisioner to write the EC2 IP to a file
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > ec2-ip.txt"
+  }
+}
 
