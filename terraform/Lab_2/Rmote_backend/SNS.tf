5824f2a41d8b70b8d549621038a94227c9b4ebdb
Mohamed K. Abonazel <mohamedabonazel4@gmail.com>
2024-12-11 00:58:19 +0200

    Update SNS.tf

--- terraform/Lab_2/Rmote_backend/SNS.tf
+++ terraform/Lab_2/Rmote_backend/SNS.tf
@@ -1,1 +1,14 @@
+# sns.tf
+
+resource "aws_sns_topic" "main" {
+  name = "high_cpu_topic"
+}
+
+resource "aws_sns_topic_subscription" "email" {
+  topic_arn = aws_sns_topic.main.arn
+  protocol  = "email"
+  endpoint  = var.sns_email  # Replace with your email address
+}
+
+
 
