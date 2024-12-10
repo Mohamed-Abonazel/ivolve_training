ebbc7ea0f54582f6dcd3b1865b493c179dd612ea
Mohamed K. Abonazel <mohamedabonazel4@gmail.com>
2024-12-11 01:09:30 +0200

    Update variables.tf

--- terraform/lab_3/Terraform-Modules/variables.tf
+++ terraform/lab_3/Terraform-Modules/variables.tf
@@ -1,20 +1,29 @@
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.1.0.0/16"
+}
+
+variable "subnet_1_cidr_block" {
+  description = "CIDR block for the first public subnet"
+  type        = string
+  default     = "10.1.1.0/24"
+}
+
+variable "subnet_2_cidr_block" {
+  description = "CIDR block for the second public subnet"
+  type        = string
+  default     = "10.1.2.0/24"
+}
+
 variable "instance_type" {
   description = "EC2 instance type"
   default     = "t2.micro"
 }
-
-variable "subnet_id" {
-  description = "The subnet ID where the EC2 instance will be deployed"
-}
-
-variable "vpc_id" {
-  description = "The VPC ID for the security group"
-}
-
-variable "name" {
-  description = "A unique name for the EC2 instance and its resources"
-  type        = string
-}
-
-
 
