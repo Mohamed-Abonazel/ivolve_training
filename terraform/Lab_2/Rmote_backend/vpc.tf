84b6d1be73a2610777ed537a73d1e04f4dc04190
Mohamed K. Abonazel <mohamedabonazel4@gmail.com>
2024-12-11 01:00:11 +0200

    Update vpc.tf

--- terraform/Lab_2/Rmote_backend/vpc.tf
+++ terraform/Lab_2/Rmote_backend/vpc.tf
@@ -1,1 +1,15 @@
+# vpc.tf
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+  enable_dns_support = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = "us-east-1a"  # Replace with your availability zone
+}
+
 
