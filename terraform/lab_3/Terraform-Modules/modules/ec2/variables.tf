afc9a794b8507b26bdd6115e99c80cbbbc45fb92
Mohamed K. Abonazel <mohamedabonazel4@gmail.com>
2024-12-11 01:14:06 +0200

    Update variables.tf

--- terraform/lab_3/Terraform-Modules/modules/ec2/variables.tf
+++ terraform/lab_3/Terraform-Modules/modules/ec2/variables.tf
@@ -1,1 +1,20 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "subnet_id" {
+  description = "The subnet ID where the EC2 instance will be deployed"
+}
+
+variable "vpc_id" {
+  description = "The VPC ID for the security group"
+}
+
+variable "name" {
+  description = "A unique name for the EC2 instance and its resources"
+  type        = string
+}
+
+
 
