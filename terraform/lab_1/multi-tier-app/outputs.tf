e9f99935e98d99382c1d13aabb9fe3ea06b24908
Mohamed K. Abonazel <mohamedabonazel4@gmail.com>
2024-12-11 00:18:37 +0200

    Update outputs.tf

--- terraform/lab_1/multi-tier-app/outputs.tf
+++ terraform/lab_1/multi-tier-app/outputs.tf
@@ -1,1 +1,16 @@
+output "vpc_id" {
+  description = "The ID of the manually created VPC"
+  value       = data.aws_vpc.ivolve-vpc.id
+}
+
+output "ec2_public_ip" {
+  description = "The public IP of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
+
+output "rds_endpoint" {
+  description = "The endpoint of the RDS instance"
+  value       = aws_db_instance.db_instance.endpoint
+}
+
 
