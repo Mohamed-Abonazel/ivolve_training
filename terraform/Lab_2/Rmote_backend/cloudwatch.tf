edde5f8f88ca1311da2f6e3015d6800da136e9de
Mohamed K. Abonazel <mohamedabonazel4@gmail.com>
2024-12-11 00:49:16 +0200

    Update cloudwatch.tf

--- terraform/Lab_2/Rmote_backend/cloudwatch.tf
+++ terraform/Lab_2/Rmote_backend/cloudwatch.tf
@@ -1,1 +1,18 @@
+# cloudwatch.tf
+
+resource "aws_cloudwatch_metric_alarm" "high_cpu" {
+  alarm_name          = "high_cpu_alarm"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = 80
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 60
+  statistic           = "Average"
+  dimensions = {
+    InstanceId = aws_instance.my_ec2.id
+  }
+
+  alarm_actions = [aws_sns_topic.main.arn]
+}
 
