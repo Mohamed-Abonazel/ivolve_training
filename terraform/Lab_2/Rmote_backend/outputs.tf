4da6bbed5a59e607608a7d77316c750da418ecbc
Mohamed K. Abonazel <mohamedabonazel4@gmail.com>
2024-12-11 00:55:23 +0200

    Update outputs.tf

--- terraform/Lab_2/Rmote_backend/outputs.tf
+++ terraform/Lab_2/Rmote_backend/outputs.tf
@@ -1,1 +1,23 @@
+# outputs.tf
+
+output "ec2_instance_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.my_ec2.public_ip
+}
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  description = "The ID of the subnet"
+  value       = aws_subnet.main.id
+}
+
+output "sns_topic_arn" {
+  description = "The ARN of the SNS topic"
+  value       = aws_sns_topic.main.arn
+}
+
 
